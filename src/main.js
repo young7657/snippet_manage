d5209b261fa692a533778ba39a337e7270d2988c
youngliu <young129@126.com>
2019-01-21 16:48:00 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+Vue.use(ElementUI)
+
+const store = new Vuex.Store({
+  state: {
+
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
---
+++ src/views/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home">
+    <div>
+      <el-button
+        type="primary"
+        icon="el-icon-plus"
+        @click="showCreateDialog">Create</el-button>
+    </div>
+    <el-row class="tac">
+      <el-col :span="6">
+        <h3>Language</h3>
+        <el-menu
+          default-active="1"
+          class="el-menu-vertical-demo">
+          <el-menu-item index="1">
+            <i class="el-icon-menu"></i>
+            <span slot="title">分类一</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-document"></i>
+            <span slot="title">分类二</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">分类三</span>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+    </el-row>
+
+    <el-dialog
+      title="Create"
+      :visible.sync="createVisible"
+      width="600px">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="createVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="createDialogSure">Sure</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      createVisible: false
+    }
+  },
+  methods: {
+    showCreateDialog() {
+      console.log(111)
+      this.createVisible = true
+    },
+    createDialogSure() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
